70451d05565856692e41e8ff1171c6f1318146bd
AOYAMA <dongqs@leyantech.com>
2026-05-12 19:46:57 +0800

    feat: symlink trick renames docker process for clean iTerm2 title (0.4.5)

--- lib/kyb.rb
+++ lib/kyb.rb
@@ -5,7 +5,7 @@ require 'fileutils'
 require 'socket'
 
 module Kyb
-  VERSION = '0.4.4'
+  VERSION = '0.4.5'
   CONFIG_FILE = File.expand_path('~/.config/kyb/config.yml')
   BASE_IMAGE = 'dev-sandbox-base'
   WORKTREE_BASE = File.expand_path('~/.kyb/worktrees')
--- lib/kyb/cli/enter.rb
+++ lib/kyb/cli/enter.rb
@@ -2,8 +2,17 @@
 
 module Kyb::CLI
   PLAY_CONTAINER = 'dev-play'
+  DOCKER = '/usr/local/bin/docker'
+  CMDS_DIR = File.expand_path('~/.cache/kyb/cmds')
 
   module_function
+
+  def docker_alias(name)
+    FileUtils.mkdir_p(CMDS_DIR)
+    path = File.join(CMDS_DIR, name)
+    File.symlink(DOCKER, path) unless File.exist?(path)
+    path
+  end
 
   def play
     Kyb::Config.load
@@ -26,7 +35,8 @@ module Kyb::CLI
 
     title = 'kyb:play'
     cmd = 'claude'
-    dexec = ['docker', 'exec', '-it', '-u', 'dev', '-w', '/home/dev']
+    d = docker_alias('kyb-play')
+    dexec = [d, 'exec', '-it', '-u', 'dev', '-w', '/home/dev']
     dexec += ['-e', "KIMI_API_KEY=#{ENV['KIMI_API_KEY']}"] if ENV['KIMI_API_KEY']
 
     if system('docker', 'exec', '-u', 'dev', PLAY_CONTAINER, 'tmux', 'has-session', '-t', 'dev',
@@ -79,7 +89,8 @@ module Kyb::CLI
 
     title = "kyb:#{container}"
     cmd = "cd ~/projects/#{name} && mise trust && claude"
-    dexec = ['docker', 'exec', '-it', '-u', 'dev', '-w', '/home/dev']
+    d = docker_alias(container.sub('dev-', 'kyb-'))
+    dexec = [d, 'exec', '-it', '-u', 'dev', '-w', '/home/dev']
     dexec += ['-e', "KIMI_API_KEY=#{ENV['KIMI_API_KEY']}"] if ENV['KIMI_API_KEY']
 
     if system('docker', 'exec', '-u', 'dev', container, 'tmux', 'has-session', '-t', 'dev',
