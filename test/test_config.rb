16e97f65418aeb53762b7fdcbffaa1a1d7ae5ee7
青山 <dongqs@leyantech.com>
2026-05-17 05:41:15 +0000

    feat: enable DinD kyb create with BuildKit + named volume worktree

--- test/test_config.rb
+++ test/test_config.rb
@@ -6,37 +6,41 @@ class ConfigTest < Minitest::Test
   end
 
   def stub_config(data)
-    Kyb::Config.define_singleton_method(:load) { @config = data }
-    Kyb::Config.define_singleton_method(:load_config) { @config = data }
-  end
-
-  def teardown
-    Kyb::Config.singleton_class.remove_method(:load) rescue nil
-    Kyb::Config.singleton_class.remove_method(:load_config) rescue nil
+    # Use Object#stub so methods are properly restored after the block.
+    # (define_singleton_method + remove_method permanently destroys module_functions.)
+    Kyb::Config.stub(:load, data) do
+    Kyb::Config.stub(:load_config, data) do
+      yield
+    end
+    end
   end
 
   def test_project_ports_as_integers
-    stub_config('projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main', 'ports' => [3000, 8080] } })
-    proj = Kyb::Config.project('niao')
-    assert_equal [3000, 8080], proj[:ports]
+    stub_config('projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main', 'ports' => [3000, 8080] } }) do
+      proj = Kyb::Config.project('niao')
+      assert_equal [3000, 8080], proj[:ports]
+    end
   end
 
   def test_project_ports_empty_when_not_set
-    stub_config('projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main' } })
-    proj = Kyb::Config.project('niao')
-    assert_equal [], proj[:ports]
+    stub_config('projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main' } }) do
+      proj = Kyb::Config.project('niao')
+      assert_equal [], proj[:ports]
+    end
   end
 
   def test_project_ports_single_value
-    stub_config('projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main', 'ports' => 3000 } })
-    proj = Kyb::Config.project('niao')
-    assert_equal [3000], proj[:ports]
+    stub_config('projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main', 'ports' => 3000 } }) do
+      proj = Kyb::Config.project('niao')
+      assert_equal [3000], proj[:ports]
+    end
   end
 
   def test_project_path_expanded
-    stub_config('projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main' } })
-    proj = Kyb::Config.project('niao')
-    assert_equal File.expand_path('~/niao'), proj[:path]
+    stub_config('projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main' } }) do
+      proj = Kyb::Config.project('niao')
+      assert_equal File.expand_path('~/niao'), proj[:path]
+    end
   end
 
   # -- extra_prompt -------------------------------------------------------
@@ -44,102 +48,116 @@ class ConfigTest < Minitest::Test
   # -- claude_default_model -----------------------------------------------
 
   def test_claude_default_model_default
-    stub_config('base' => {})
-    assert_equal 'flash', Kyb::Config.claude_default_model
+    stub_config('base' => {}) do
+      assert_equal 'flash', Kyb::Config.claude_default_model
+    end
   end
 
   def test_claude_default_model_custom
-    stub_config('base' => { 'claude_default_model' => 'pro' })
-    assert_equal 'pro', Kyb::Config.claude_default_model
+    stub_config('base' => { 'claude_default_model' => 'pro' }) do
+      assert_equal 'pro', Kyb::Config.claude_default_model
+    end
   end
 
   # -- proxy --------------------------------------------------------------
 
   def test_proxy_default_nil
-    stub_config('base' => {})
-    assert_nil Kyb::Config.proxy
-    assert_nil Kyb::Config.no_proxy
+    stub_config('base' => {}) do
+      assert_nil Kyb::Config.proxy
+      assert_nil Kyb::Config.no_proxy
+    end
   end
 
   def test_proxy_base_level
-    stub_config('base' => { 'proxy' => 'socks5://host:2080', 'no_proxy' => '.internal' })
-    assert_equal 'socks5://host:2080', Kyb::Config.proxy
-    assert_equal '.internal', Kyb::Config.no_proxy
+    stub_config('base' => { 'proxy' => 'socks5://host:2080', 'no_proxy' => '.internal' }) do
+      assert_equal 'socks5://host:2080', Kyb::Config.proxy
+      assert_equal '.internal', Kyb::Config.no_proxy
+    end
   end
 
   def test_project_proxy_inherits_from_base
     stub_config('base' => { 'proxy' => 'socks5://host:2080', 'no_proxy' => '.internal' },
-                'projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main' } })
-    proj = Kyb::Config.project('niao')
-    assert_equal 'socks5://host:2080', proj[:proxy]
-    assert_equal '.internal', proj[:no_proxy]
+                'projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main' } }) do
+      proj = Kyb::Config.project('niao')
+      assert_equal 'socks5://host:2080', proj[:proxy]
+      assert_equal '.internal', proj[:no_proxy]
+    end
   end
 
   def test_project_proxy_overrides_base
     stub_config('base' => { 'proxy' => 'socks5://host:2080', 'no_proxy' => '.internal' },
-                'projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main', 'proxy' => 'http://local:3128', 'no_proxy' => '' } })
-    proj = Kyb::Config.project('niao')
-    assert_equal 'http://local:3128', proj[:proxy]
-    assert_equal '', proj[:no_proxy]
+                'projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main', 'proxy' => 'http://local:3128', 'no_proxy' => '' } }) do
+      proj = Kyb::Config.project('niao')
+      assert_equal 'http://local:3128', proj[:proxy]
+      assert_equal '', proj[:no_proxy]
+    end
   end
 
   # -- sandbox_allowed_domains --------------------------------------------
 
   def test_sandbox_allowed_domains_defaults
-    stub_config('projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main' } })
-    proj = Kyb::Config.project('niao')
-    assert_includes proj[:sandbox_allowed_domains], '*.npmjs.org'
-    assert_includes proj[:sandbox_allowed_domains], 'github.com'
-    assert_includes proj[:sandbox_allowed_domains], 'localhost'
+    stub_config('projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main' } }) do
+      proj = Kyb::Config.project('niao')
+      assert_includes proj[:sandbox_allowed_domains], '*.npmjs.org'
+      assert_includes proj[:sandbox_allowed_domains], 'github.com'
+      assert_includes proj[:sandbox_allowed_domains], 'localhost'
+    end
   end
 
   def test_sandbox_allowed_domains_with_extra
-    stub_config('projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main', 'sandbox_allowed_domains' => ['*.internal.corp.com'] } })
-    proj = Kyb::Config.project('niao')
-    assert_includes proj[:sandbox_allowed_domains], '*.npmjs.org'
-    assert_includes proj[:sandbox_allowed_domains], '*.internal.corp.com'
+    stub_config('projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main', 'sandbox_allowed_domains' => ['*.internal.corp.com'] } }) do
+      proj = Kyb::Config.project('niao')
+      assert_includes proj[:sandbox_allowed_domains], '*.npmjs.org'
+      assert_includes proj[:sandbox_allowed_domains], '*.internal.corp.com'
+    end
   end
 
   # -- timezone ------------------------------------------------------------
 
   def test_timezone_default
-    stub_config('projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main' } })
-    proj = Kyb::Config.project('niao')
-    assert_equal 'Asia/Shanghai', proj[:timezone]
+    stub_config('projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main' } }) do
+      proj = Kyb::Config.project('niao')
+      assert_equal 'Asia/Shanghai', proj[:timezone]
+    end
   end
 
   def test_timezone_custom
-    stub_config('projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main', 'timezone' => 'America/New_York' } })
-    proj = Kyb::Config.project('niao')
-    assert_equal 'America/New_York', proj[:timezone]
+    stub_config('projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main', 'timezone' => 'America/New_York' } }) do
+      proj = Kyb::Config.project('niao')
+      assert_equal 'America/New_York', proj[:timezone]
+    end
   end
 
   # -- extra_prompt -------------------------------------------------------
 
   def test_extra_prompt_nil_when_not_set
-    stub_config('base' => {}, 'projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main' } })
-    proj = Kyb::Config.project('niao')
-    assert_nil proj[:extra_prompt]
+    stub_config('base' => {}, 'projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main' } }) do
+      proj = Kyb::Config.project('niao')
+      assert_nil proj[:extra_prompt]
+    end
   end
 
   def test_extra_prompt_from_base
     stub_config('base' => { 'extra_prompt' => '全局规则' },
-                'projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main' } })
-    proj = Kyb::Config.project('niao')
-    assert_equal '全局规则', proj[:extra_prompt]
+                'projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main' } }) do
+      proj = Kyb::Config.project('niao')
+      assert_equal '全局规则', proj[:extra_prompt]
+    end
   end
 
   def test_extra_prompt_from_project
     stub_config('base' => {},
-                'projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main', 'extra_prompt' => '项目规则' } })
-    proj = Kyb::Config.project('niao')
-    assert_equal '项目规则', proj[:extra_prompt]
+                'projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main', 'extra_prompt' => '项目规则' } }) do
+      proj = Kyb::Config.project('niao')
+      assert_equal '项目规则', proj[:extra_prompt]
+    end
   end
 
   def test_extra_prompt_merged
     stub_config('base' => { 'extra_prompt' => '全局规则' },
-                'projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main', 'extra_prompt' => '项目规则' } })
-    proj = Kyb::Config.project('niao')
-    assert_equal '全局规则 项目规则', proj[:extra_prompt]
+                'projects' => { 'niao' => { 'path' => '~/niao', 'base_branch' => 'main', 'extra_prompt' => '项目规则' } }) do
+      proj = Kyb::Config.project('niao')
+      assert_equal '全局规则 项目规则', proj[:extra_prompt]
+    end
   end
 end
