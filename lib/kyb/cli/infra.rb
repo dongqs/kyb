d7f8aba5d415920e033d92040f3eaf101fcae842
AOYAMA <dongqs@leyantech.com>
2026-05-23 00:45:31 +0800

    fix: set KYB_PROJECT=kyb in infra-boss for proper entrypoint setup

--- lib/kyb/cli/infra.rb
+++ lib/kyb/cli/infra.rb
@@ -64,6 +64,8 @@ module Kyb::CLI
     args += ['-e', "HOST_GID=#{Process.gid}"]
     args += ['-e', "GITLAB_TOKEN=#{ENV['GITLAB_TOKEN']}"] if ENV['GITLAB_TOKEN']
     args += ['-e', "ALL_PROXY=socks5://kyb-infra-sing-box:2080"]
+    args += ['-e', 'KYB_PROJECT=kyb']
+    args += ['-e', 'KYB_BRANCH=infra-boss']
 
     # Standard mounts (like kyb create)
     home = ENV['HOME']
